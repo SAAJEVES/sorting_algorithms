8b5f3556a87222e432e2866655e7de93c92a0c9b
SAAJEVES <eveshofase@gmail.com>
2023-10-23 04:14:45 +0100

    bubble2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,7 +27,7 @@ void bubble_sort(int *array, size_t size)
 			for (j = 0; j < (size - i - 1); j++)
 			{
 				temp = array[j];
-				if (array[j] > array[j+1])
+				if (array[j] > array[j + 1])
 				{
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
@@ -37,7 +37,6 @@ void bubble_sort(int *array, size_t size)
 			}
 			if (!swapped)
 				break;
-			/* print_array(array, size); */
 		}
 	}
 }
