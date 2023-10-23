f7dfd39dafa2ea7aec546c4ac8e829aeb581d179
SAAJEVES <eveshofase@gmail.com>
2023-10-23 04:11:50 +0100

    bubble1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,17 +13,31 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	bool swapped;
 
-	for (i = 0; i < size; i++)
+	if (size < 2)
 	{
-		for (j = 0; j < (size - 1); j++)
+		return;
+	}
+	else
+	{
+		for (i = 0; i < (size - 1); i++)
 		{
-			temp = array[j];
-			if (array[j] > array[j+1])
+			swapped = false;
+			for (j = 0; j < (size - i - 1); j++)
 			{
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = array[j];
+				if (array[j] > array[j+1])
+				{
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					swapped = true;
+				}
+				print_array(array, size);
 			}
+			if (!swapped)
+				break;
+			/* print_array(array, size); */
 		}
 	}
 }
--- sort.h
+++ sort.h
@@ -3,7 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
